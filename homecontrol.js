68a4ffae10d95fb1fa886954dd5021a415cec9df
Rene <info@rg-engineering.eu>
2016-08-18 20:12:57 +0200

    version 0.0.2
    runs at least for one sensor

--- homecontrol.js
+++ homecontrol.js
@@ -10,7 +10,8 @@ var utils =    require(__dirname + '/lib/utils'); // Get common adapter utils
 // adapter will be restarted automatically every time as the configuration changed, e.g system.adapter.template.0
 var adapter = utils.adapter('homecontrol');
 
-var devices = {};
+var objects = {};
+var metaRoles = {};
 
 var myPort = null;
 
@@ -35,7 +36,7 @@ adapter.on('unload', function (callback) {
 // is called if a subscribed object changes
 adapter.on('objectChange', function (id, obj) {
     // Warning, obj can be null if it was deleted
-    adapter.log.info('objectChange ' + id + ' ' + JSON.stringify(obj));
+    adapter.log.debug('objectChange ' + id + ' ' + JSON.stringify(obj));
 
     //feuert auch, wenn adapter im admin anghalten oder gestartet wird...
 
@@ -48,7 +49,7 @@ adapter.on('objectChange', function (id, obj) {
 // is called if a subscribed state changes
 adapter.on('stateChange', function (id, state) {
     // Warning, state can be null if it was deleted
-    adapter.log.info('stateChange ' + id + ' ' + JSON.stringify(state));
+    adapter.log.debug('stateChange ' + id + ' ' + JSON.stringify(state));
 
     // you can use the ack flag to detect if it is status (true) or command (false)
     if (state && !state.ack) {
@@ -96,58 +97,63 @@ function main() {
         console.warn('Serial port is not created');
     }
 
+    /*
+    adapter.objects.getObject('homecontrol.meta.roles', function (err, res) {
+        metaRoles = res.native;
+        adapter.objects.getObjectView('homecontrol', 'devices', function (err, res) {
+            for (var i = 0, l = res.total_rows; i < l; i++) {
+                objects[res.rows[i].id] = res.rows[i].value;
+            }
+        });
+    */
     myPort.on('open', showPortOpen);
     myPort.on('data', sendSerialData);
     myPort.on('close', showPortClose);
     myPort.on('error', showError);
 
-    adapter.log.info('port created; portname: ' + options.serialport);
-    adapter.log.info('Data rate: ' + myPort.options.baudRate + ' ' + options.baudrate);
+    adapter.log.debug('port created; portname: ' + options.serialport);
+    adapter.log.debug('Data rate: ' + myPort.options.baudRate + ' ' + options.baudrate);
 
-
+//test only =====================================================
+ /*   adapter.delObject("CE1283180000",function (err, obj) {
+	    if (err) {
+	        adapter.log.error(err);
+	        return -1;
+	    }
+	    else
+	    {
+	    	adapter.log.info("object deleted");
+	    }
+    }
     
-
+    		);
+    		*/
+//until here =====================================================
+    
 }
 
 
 function showPortOpen() {
     if (myPort != null) {
-        adapter.log.info('port open. ');
+        adapter.log.debug('port open. ');
         myPort.write("V\n\r");
         myPort.write("hr\n\r");
     }
 }
 
-/*
+
 function insertObjects(objs) {
     if (objs.length < 1) {
         return;
     } else {
         var newObject = objs.pop();
         adapter.setObject(newObject._id, newObject, function (err, res) {
-            adapter.log.info('object ' + adapter.namespace + '.' + newObject._id + ' created');
+            adapter.log.debug('object ' + adapter.namespace + '.' + newObject._id + ' created');
             setTimeout(insertObjects, 0, objs);
         });
     }
 }
-*/
 
-
-function findDevice(sourceId) {
-
-    return -1;
-}
-
-function addDevice(sourceId) {
-
-    var obj;
-
-
-    adapter.setObject(obj._id, obj, function (err) {
-        if (err) adapter.log.error(err);
-    })
-
-}
 
 function sendSerialData(data) {
     data = data.toString();
@@ -155,10 +161,10 @@ function sendSerialData(data) {
     if (data.length < 2) {
         return;
     }
-    adapter.log.info(data);
+    adapter.log.debug(data);
 
     if (data.includes("receive off")) {
-        adapter.log.info('port reopen. ');
+        adapter.log.debug('port reopen. ');
         myPort.write("V\n\r");
         myPort.write("hr\n\r");
         return;
@@ -170,23 +176,105 @@ function sendSerialData(data) {
 
     //got data from Sensor :3FAF82180000 with 2 DP as broadcast Temp 30.64 C Press 958.32 mBar
     try {
-
         var res = data.split(" ");
-        var source = res[4].substr(1); //remove first character
+        var id = res[4].substr(1); //remove first character
+        var type = res[3].substr(0);
         var datapoints = parseInt(res[7].substr(0));
-        adapter.log.info("from " + source + " with " + datapoints);
+        //adapter.log.info("from " + id + " with " + datapoints);
+        
+        //adapter.log.info("split size " + res.length);
+        
 
-        //for (i = 0; i < datapoints; i++) {
-        //}
-
-
-
-        if (findDevice(source) == -1) {
-            addDevice(source);
+        
+        //got data from Sensor :CE1283180000 with 1 DP as broadcast Bright 0 lux
+        //got data from Sensor :CE1283180000 with 2 DP as broadcast Temp 24.80 C Hum 57.50 %
+        //got data from Sensor :CE1283180000 with 2 DP as broadcast Temp 24.67 C Press 962.35 mBar 
+    	//got data from Sensor :CE1283180000 with 2 DP as broadcast Temp 24.66 C Press 962.33 mBar 
+        //                           4            7                  11
+        var state2add=false;
+        for (var _i = 0; _i < datapoints; _i++) {
+        	var _idx=11 + (_i*5);
+        	//adapter.log.info("index " + _idx);
+        	var _state = res[_idx];
+        	
+        	var _value;
+        	var found=false;
+        	var k=1;
+        	while (!found)
+        	{
+        		_value = parseFloat(res[_idx+k].substr(0));
+        		
+        		k++;
+        		if (!isNaN(_value)) //check NaN
+        		{
+        			found = true;
+        		}
+        		
+        		else if (k>5)
+        		{
+        			adapter.log.warn("value not found")
+        			found = true;
+        		}
+        	}
+        	
+        	
+        	//adapter.log.info(_idx + " : " + res[_idx-1] + " , " + res[_idx] + " value " + _value);
+        	
+        	if (!objects[id + '.' + _state])
+        	{
+        		//add new states if not exists
+        		state2add=true;
+        		adapter.log.debug(id + '.' + _state + " not found; try to add");
+        	}
+        	
+        	//just update value for all states
+            adapter.setState(id + '.' + _state, {val: _value, ack: true});
         }
+        
+        //add new states
+        if (!objects[id] || state2add){
+        	adapter.log.info("add new device " + id);
+        	var newObjects = [];
+        	var newDevice = {
+                    _id:    id,
+                    type:   'device',
+                    common: {
+                        name: type
+                    },
+                    native: ""
+                };
+        	
+            for (var i = 0; i < datapoints; i++) {
+            	var common = {};
+            	var idx=11 + i*5;
+            	var _state = res[idx];
+            	//var value = parseFloat(res[12+ i*3].substr(0));
+            	//var unit = res[13+i*3];
+            	
+                if (metaRoles[id + '_' + _state]) {
+                    common = metaRoles[id + '_' + _state];
+                } else if (metaRoles[_state]) {
+                    common = metaRoles[_state];
+                }
 
+                common.name = _state;
 
+                adapter.log.debug("add new state " + id + '.' + _state);
+                var newState = {
+                    _id:    id + '.' + _state,
+                    type:   'state',
+                    common: common,
+                    native: {}
+                };
 
+                objects[id + '.' + _state] = newState;
+                newObjects.push(newState);
+            }
+            objects[id] = newDevice;
+            newObjects.push(newDevice);
+
+            insertObjects(newObjects);
+        }
     }
 
     catch (e) {
@@ -195,7 +283,7 @@ function sendSerialData(data) {
 }
 
 function showPortClose() {
-    adapter.log.info('port closed.');
+    adapter.log.debug('port closed.');
 }
 
 function showError(error) {
