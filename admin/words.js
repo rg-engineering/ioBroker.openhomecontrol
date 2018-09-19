14e0bb9570cddaf7357b26ca24185c2b8ad7bd17
Rene <info@rg-engineering.eu>
2018-09-19 20:35:54 +0200

    rework and update to admin3

--- admin/words.js
+++ admin/words.js
@@ -1,10 +1,131 @@
-﻿'use strict';
+﻿/*global systemDictionary:true */
+'use strict';
 
 systemDictionary = {
-    "myHomeControl adapter settings": { "de": "myHomeControl Adapter Einstellungen" },
-    "on save adapter restarts with new config immediately": { "de": "Beim Speichern von Einstellungen wird der Adapter sofort neu gestartet." },
-    "select Device": { "de": "Geräteauswahl" },
-    "Serial port": { "de": "serieller Port" },
-    "Baud rate": { "de": "Übertragungsrate" },
-    "send Interval broadcast [s]": { "de": "Sendeintervall broadcast [s]" }
+
+
+    "disable_broadcast_hint":
+        {
+            "en": "to disable sending broadcast data set interval to -1; 0 will use hardcoded standard value.",
+            "de": "um das Senden des Broadcast-Datensatzintervalls auf -1 zu deaktivieren; 0 wird den fest codierten Standardwert verwenden.",
+            "ru": "для отключения отправки интервала набора данных передачи до -1; 0 будет использовать стандартное значение hardcoded.",
+            "pt": "para desabilitar o envio do intervalo do conjunto de dados de transmissão para -1; 0 usará o valor padrão codificado.",
+            "nl": "om het verzenden van uitzendgegevensinterval naar -1 uit te schakelen; 0 gebruikt de standaardwaarde met harde codes.",
+            "fr": "pour désactiver l'envoi de l'intervalle de données diffusé à -1; 0 utilisera une valeur standard codée en dur.",
+            "it": "per disabilitare l'invio dell'intervallo di set di dati di trasmissione a -1; 0 utilizzerà il valore standard hardcoded.",
+            "es": "deshabilitar el envío del intervalo del conjunto de datos de difusión a -1; 0 usará el valor estándar codificado.",
+            "pl": "wyłączyć interwał czasowy wysyłania transmisji do -1; 0 użyje zakodowanej wartości standardowej."
+        },
+    "select_device":
+        {
+            "en": "select Device",
+            "de": "Wählen Sie Gerät",
+            "ru": "выберите устройство",
+            "pt": "Selecionar dispositivo",
+            "nl": "selecteer apparaat",
+            "fr": "Choisir un appareil",
+            "it": "seleziona dispositivo",
+            "es": "seleccione el dispositivo",
+            "pl": "Wybierz urządzenie"
+        },
+    "select_mode":
+        {
+            "en": "select Mode",
+            "de": "Auswahlmodus",
+            "ru": "выберите режим",
+            "pt": "modo de seleção",
+            "nl": "Selecteer modus",
+            "fr": "sélectionnez le mode",
+            "it": "selezionare Modalità",
+            "es": "Seleccionar modo",
+            "pl": "Wybierz tryb"
+        },
+    "serialport":
+        {
+            "en": "serialport",
+            "de": "serielle Schnittstelle",
+            "ru": "Серийный порт",
+            "pt": "porta serial",
+            "nl": "seriele poort",
+            "fr": "port série",
+            "it": "porta seriale",
+            "es": "Puerto serial",
+            "pl": "Port szeregowy"
+        },
+    "baudrate":
+        {
+            "en": "baudrate",
+            "de": "Baudrate",
+            "ru": "бод",
+            "pt": "taxa de transmissão",
+            "nl": "baudrate",
+            "fr": "baudrate",
+            "it": "baudrate",
+            "es": "velocidad de baudios",
+            "pl": "szybkość transmisji"
+        },
+    "send_interval_broadcast":
+        {
+            "en": "send Interval broadcast [s]",
+            "de": "Sende Interval Broadcast [s]",
+            "ru": "отправить Интервал трансляции [s]",
+            "pt": "Enviar transmissão por intervalo [s]",
+            "nl": "verzend Interval uitzending [s]",
+            "fr": "envoi d'intervalles [s]",
+            "it": "invia una trasmissione intervallata [s]",
+            "es": "enviar transmisión por intervalos [s]",
+            "pl": "wyślij transmisję Interwał [s]"
+        },
+    "option_cul":
+        {
+            "en": "CUL (needs a modified culfw)",
+            "de": "CUL (braucht einen modifizierten culfw)",
+            "ru": "CUL (требуется модифицированный куб)",
+            "pt": "CUL (precisa de um culfw modificado)",
+            "nl": "CUL (heeft een aangepaste culfw nodig)",
+            "fr": "CUL (nécessite un procédé modifié)",
+            "it": "CUL (ha bisogno di una modifica modificata)",
+            "es": "CUL (necesita un culfw modificado)",
+            "pl": "CUL (potrzebuje zmodyfikowanego culfw)"
+        },
+    "option_homeControl":
+        {
+            "en": "HomeControl",
+            "de": "HomeControl",
+            "ru": "HomeControl",
+            "pt": "HomeControl",
+            "nl": "HomeControl",
+            "fr": "HomeControl",
+            "it": "homecontrol",
+            "es": "HomeControl",
+            "pl": "HomeControl"
+        },
+    "option_rawdata":
+        {
+            "en": "interpretes raw data(default)",
+            "de": "interpretiert Rohdaten (Standard)",
+            "ru": "интерпретирует необработанные данные (по умолчанию)",
+            "pt": "interpreta dados brutos (padrão)",
+            "nl": "interpreteert onbewerkte gegevens (standaard)",
+            "fr": "interprète les données brutes (par défaut)",
+            "it": "interpreta i dati grezzi (predefinito)",
+            "es": "interpreta datos sin procesar (predeterminado)",
+            "pl": "interpretuje dane surowe (domyślne)"
+        },
+    "option_telegramdata":
+        {
+            "en": "interpretes telegram data(obsolete)",
+            "de": "interpretiert Telegrammdaten (veraltet)",
+            "ru": "интерпретирует данные телеграммы (устаревшие)",
+            "pt": "interpreta dados de telegrama (obsoletos)",
+            "nl": "interpreteert telegramgegevens (verouderd)",
+            "fr": "interprète les données de télégrammes (obsolètes)",
+            "it": "interpreta i dati del telegramma (obsoleto)",
+            "es": "interpreta datos de telegramas (obsoletos)",
+            "pl": "interpretuje dane telegramów (przestarzałe)"
+        }
+
+
+    
+    
 };
